9b2cdec03284d3a63a82da53fdfee794ec57e011
Pawel024 <p.j.rok@student.tudelft.nl>
2024-06-23 15:42:48 +0200

    another small fix

--- src/startpage/level.js
+++ src/startpage/level.js
@@ -11,23 +11,17 @@ function Level({ level, levelNames, taskNames, introData, quizData, introsByLeve
         {showContent && (
             <GridBox>
                 {introsByLevel[level] && introsByLevel[level].map((intro, index) => (
-                    <>
-                    { introData.find(entry => entry.intro_id === 10*level+intro).visibility &&
-                    <ChallengeButton link={`introduction${level}${intro}`} label="Introduction" Icon={RocketIcon}/>
-                    }
-                    </>
+                    introData.find(entry => entry.intro_id === 10*level+intro).visibility &&
+                    <ChallengeButton key={`intro${level}${intro}_button`} link={`introduction${level}${intro}`} label="Introduction" Icon={RocketIcon}/>
                 ))}
 
                 {challenges.map((challenge, index) => (
-                    <ChallengeButton link={`challenge${level}${challenge}`} label={taskNames[`${level}${challenge}`]} Icon={RocketIcon} />
+                    <ChallengeButton key={`challenge${level}${challenge}_button`} link={`challenge${level}${challenge}`} label={taskNames[`${level}${challenge}`]} Icon={RocketIcon} />
                 ))}
 
                 {quizzesByLevel[level] && quizzesByLevel[level].map((quiz, index) => (
-                    <>
-                    { quizData.find(entry => entry.quiz_id === 10*level+quiz).visibility &&
-                        <ChallengeButton link={`quiz${level}${quiz}`} label="Quiz" Icon={Pencil2Icon} />
-                    }
-                    </>
+                    quizData.find(entry => entry.quiz_id === 10*level+quiz).visibility &&
+                    <ChallengeButton key={`quiz${level}${quiz}_button`} link={`quiz${level}${quiz}`} label="Quiz" Icon={Pencil2Icon} />
                 ))}
             </GridBox>
         )}
