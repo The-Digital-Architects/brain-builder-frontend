d2aede94a05696b5dca1c1379c528aa2af83441b
Laurens-Ledeganck <laurens.ledeganck@gmail.com>
2024-07-05 13:41:35 +0200

    JupyterLite demo was successful, first attempt at using a custom notebook

--- src/jupyterLiteView.js
+++ src/jupyterLiteView.js
@@ -7,7 +7,7 @@ class JupyterLite extends React.Component {
             <div>
                 <Header showHomeButton={true} />
                 <iframe
-                    src="https://jupyterlite.github.io/demo/lab/index.html"  // TODO: change this to the right URL
+                    src="/staticfiles/_output/lab/index.html"  // TODO: change this to the right URL
                     style={{  // TODO: check style
                         width: '100%',
                         height: 'calc(100vh - 50px)', 
