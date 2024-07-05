f16f338e510f447822e17c8c6869322e5097047c
Laurens-Ledeganck <laurens.ledeganck@gmail.com>
2024-07-05 17:56:23 +0200

    In previous commit the jupyterlite command was successful, but still 404 not found and no trace of it in the CLI. Now trying to build it locally and add jupyter_lite as a staticfile directory.

--- src/jupyterLiteView.js
+++ src/jupyterLiteView.js
@@ -7,7 +7,7 @@ class JupyterLite extends React.Component {
             <div>
                 <Header showHomeButton={true} />
                 <iframe
-                    src="/static/jupyter/lab/index.html"  // TODO: change this to the right URL
+                    src= '/static/jupyter/index.html'  // TODO: change this to the right URL
                     style={{  // TODO: check style
                         width: '100%',
                         height: 'calc(100vh - 50px)', 
