62d38753ae563af7afce00340420ece131729a1e
Laurens-Ledeganck <laurens.ledeganck@gmail.com>
2024-07-06 11:11:05 +0200

    Bugfix

--- src/jupyterLiteView.js
+++ src/jupyterLiteView.js
@@ -7,7 +7,7 @@ class JupyterLite extends React.Component {
             <div>
                 <Header showHomeButton={true} />
                 <iframe
-                    src= '/static/jupyter_lite/index.html'  // TODO: change this to the right URL
+                    src= '/static/jupyter_lite/_output/index.html'  // TODO: change this to the right URL
                     style={{  // TODO: check style
                         width: '100%',
                         height: 'calc(100vh - 50px)', 
