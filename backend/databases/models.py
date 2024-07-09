7839eda4e3a07a070b027e238e83f6537c7ddd3b
Laurens-Ledeganck <laurens.ledeganck@gmail.com>
2024-07-09 21:17:00 +0200

    Let's try that again

--- backend/databases/models.py
+++ backend/databases/models.py
@@ -14,7 +14,7 @@ class Row(models.Model):
 
 class Progress(models.Model):
     user_id = models.CharField(max_length=100)
-    task_description = models.ForeignKey('TaskDescription', on_delete=models.CASCADE, related_name='progress')
+    task_description = models.ForeignKey('TaskDescription', on_delete=models.CASCADE, related_name='progress', null=True)
     status = models.IntegerField()
     timestamp = models.DateTimeField(auto_now=True)
 
