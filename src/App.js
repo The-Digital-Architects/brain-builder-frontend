8f7ba0c6faba1c2b4622d9a7372d943a8e321836
Laurens-Ledeganck <laurens.ledeganck@gmail.com>
2024-07-11 00:17:46 +0200

    Debugging

--- src/App.js
+++ src/App.js
@@ -461,7 +461,7 @@ function App() {
         setInitPlots(currentTaskIds.map(() => []));
 
           // some custom taskIds
-        setManualRegressionId(taskNames.find(task => task === 'Linear Regr.'));
+        setManualRegressionId(Object.keys(taskNames).find(key => taskNames[key] === 'Linear Regr.'));
         setManualRegressionDescription(taskData.find(task => task.task_id === manualRegressionId).description);
         console.log("manualRegressionId & Description: ", manualRegressionId, manualRegressionDescription); // TODO remove this
 
