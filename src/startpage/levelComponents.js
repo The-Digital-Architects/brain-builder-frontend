ac8b0b5bc8e8c15546bfde893fb202e0363016e8
Pawel024 <p.j.rok@student.tudelft.nl>
2024-07-12 13:19:25 +0200

    make TextField show up

--- src/startpage/levelComponents.js
+++ src/startpage/levelComponents.js
@@ -214,8 +214,9 @@ function ProgressBox({progress}) {
 
         <label style={{paddingTop: 5, fontSize: 'var(--font-size-2)'}}>Do you already have a code? Enter it below!</label>
         <Box maxWidth="250px">
-          <TextField.Root variant="soft" size="2" placeholder="Paste and click Enter to save (test)"/>
-          {/* onChange={handleCodeChange} onKeyDown={handleSubmit} />*/}
+          <TextField.Root variant="soft" size="2">
+            <TextField.Input placeholder="Paste and click Enter to save (test)" onChange={handleCodeChange} onKeyDown={handleSubmit} />
+          </TextField.Root>
         </Box>
       </Flex>
     </Box>
