4bb8951c513a1d556947dd5ba7ccd5e432d9a5e4
Pawel024 <p.j.rok@student.tudelft.nl>
2024-08-11 01:09:37 +0200

    bug fix

--- src/otherTasks.js
+++ src/otherTasks.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import './css/App.css';
 import { Flex, Theme, Box, Heading, Separator } from '@radix-ui/themes';
 import Header from './common/header';
+import * as Slider from '@radix-ui/react-slider';
 
-import { weightSlider, angleSlider, biasSlider, orderSlider, nObjectsSlider, nFeaturesSlider } from './utils/otherTasks/emissionSliders';
 import renderEmissions from './utils/otherTasks/emissionUtils';
 
 import renderMatrix from './utils/otherTasks/matrixUtils';
+
 
 class OtherTask extends Component {
     /* This component can be used for simple tasks with a split screen with an explanation on the left and sliders and a visualisation on the right, separated by a vertical line. */
@@ -196,6 +197,108 @@ class OtherTask extends Component {
     }
 
     animation() {
+
+        const weightSlider = (
+            <Slider.Root
+              className="SliderRoot"
+              defaultValue={[45]}
+              onValueChange={(value) => this.handleWeightChange(value)}
+              min={-85}
+              max={85}
+              step={1}
+              style={{ width: Math.round(0.16 * (window.innerWidth * 0.97)), margin: 10 }}
+            >
+              <Slider.Track className="SliderTrack" style={{ height: 3 }}>
+                <Slider.Range className="SliderRange" />
+              </Slider.Track>
+              <Slider.Thumb className="SliderThumb" aria-label="Weight" />
+            </Slider.Root>
+          );
+        
+        const angleSlider = (
+            <Slider.Root
+              className="SliderRoot"
+              defaultValue={[0]}
+              onValueChange={(value) => this.handleAngleChange(value)}
+              min={-180}
+              max={180}
+              step={1}
+              style={{ width: Math.round(0.16 * (window.innerWidth * 0.97)), margin: 10 }}
+            >
+              <Slider.Track className="SliderTrack" style={{ height: 3 }}>
+                <Slider.Range className="SliderRange" />
+              </Slider.Track>
+              <Slider.Thumb className="SliderThumb" aria-label="Weight" />
+            </Slider.Root>
+          );
+        
+        const biasSlider = (
+            <Slider.Root
+              className="SliderRoot"
+              defaultValue={[0]}
+              onValueChange={(value) => this.handleBiasChange(value)}
+              min={-5}
+              max={5}
+              step={0.01}
+              style={{ width: Math.round(0.16 * (window.innerWidth * 0.97)), margin: 10 }}
+            >
+              <Slider.Track className="SliderTrack" style={{ height: 3 }}>
+                <Slider.Range className="SliderRange" />
+              </Slider.Track>
+              <Slider.Thumb className="SliderThumb" aria-label="Bias" />
+            </Slider.Root>
+        );  
+        
+        const orderSlider = (
+            <Slider.Root
+                className="SliderRoot"
+                defaultValue={[1]}
+                onValueChange={(value) => this.handleOrderChange(value)}
+                min={1}
+                max={10}
+                step={1}
+                style={{ width: Math.round(0.16 * (window.innerWidth * 0.97)), margin: 10 }}
+            >
+            <Slider.Track className="SliderTrack" style={{ height: 3 }}>
+                <Slider.Range className="SliderRange" />
+              </Slider.Track>
+              <Slider.Thumb className="SliderThumb" aria-label="Order" />
+            </Slider.Root>
+        );
+        
+        const nObjectsSlider = (
+            <Slider.Root
+                className="SliderRoot"
+                defaultValue={[5]}
+                onValueChange={(value) => this.handleMatrixChange(value, 1)}
+                min={2}
+                max={20}
+                step={1}
+                style={{ width: Math.round(0.16 * (window.innerWidth * 0.97)), margin: 10 }}
+            >
+            <Slider.Track className="SliderTrack" style={{ height: 3 }}>
+                <Slider.Range className="SliderRange" />
+              </Slider.Track>
+              <Slider.Thumb className="SliderThumb" aria-label="nObjects" />
+            </Slider.Root>
+        );
+        
+        const nFeaturesSlider = (
+            <Slider.Root
+                className="SliderRoot"
+                defaultValue={[3]}
+                onValueChange={(value) => this.handleMatrixChange(value, 2)}
+                min={1}
+                max={10}
+                step={1}
+                style={{ width: Math.round(0.16 * (window.innerWidth * 0.97)), margin: 10 }}
+            >
+            <Slider.Track className="SliderTrack" style={{ height: 3 }}>
+                <Slider.Range className="SliderRange" />
+              </Slider.Track>
+              <Slider.Thumb className="SliderThumb" aria-label="nFeatures" />
+            </Slider.Root>
+        );
 
         return (
             <Box style={{ flex:1, display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: window.innerHeight-52, padding:'30px 50px',  }}>
