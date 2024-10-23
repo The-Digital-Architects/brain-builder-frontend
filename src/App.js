d954eaf414b4601672067a367de80e1bb0fee0ef
Pawel024 <p.j.rok@student.tudelft.nl>
2024-10-22 21:56:47 -0700

    logging

--- src/App.js
+++ src/App.js
@@ -464,6 +464,7 @@ function App() {
         setFileNames(currentFileNames);
         setFunctionNames(currentFunctionNames);
         setGamesData(JSON.stringify(currentTaskData));
+        console.log('gamesData: ', gamesData)  // TODO remove
         setNInputs(currentNInputs);
         setNOutputs(currentNOutputs);
         setNObjects(currentTaskIds.map(() => 0));
